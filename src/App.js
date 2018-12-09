a3f626a717dfe8459c6fa23bd4576432f189680d
AlCast <alvaro.castelan.21@gmail.com>
2018-12-08 23:36:41 -0600

    set sass + react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Router, Route, Switch} from "react-router-dom";
+import history from "./history"
+import Login from "./pages/Login";
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Spotify Web App</p>
-          
-      </div>
+      // <div className="App">
+      //   <p>Spotify Web App</p>
+      //   <a href='http://localhost:8888'> Login to Spotify </a>
+        
+      // </div>
+      <React.Fragment>
+        <Router history={history}>
+          <React.Fragment>
+            <Switch>
+              <Route exact path="/login" component={Login}/>
+            </Switch>
+          </React.Fragment>
+        </Router>
+      </React.Fragment>
     );
   }
 }
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class LogIn extends Component {
+  render() {
+    return (
+      <div className="log-in">
+      <p>Hello World</p>
+        
+      </div>
+    )
+  }
+}
+
+export default LogIn
