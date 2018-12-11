097f39195a16aabcf7808ec64d7871ebc6c2a336
AlCast <alvaro.castelan.21@gmail.com>
2018-12-10 22:25:44 -0600

    added table component to visualize top 50 songs long term

---
+++ src/components/TracksTable/TracksTable.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Table} from "reactstrap";
+const TracksTable = (props) => {
+  return (
+    <div className="tracks-table">
+    <Table hover>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Energy</th>
+                <th>Dance</th>
+                <th>Valence</th>
+                <th>Acoustic</th>
+                <th>Pop.</th>
+
+
+            </tr>
+        </thead>
+        <tbody>
+            {props.tracks}
+        </tbody>
+    </Table>
+      
+    </div>
+  )
+}
+
+export default TracksTable
--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react'
 
 import SpotifyWebApi from "spotify-web-api-js";
 import NavBar from "../../components/NavBar";
+import TracksTable from "../../components/TracksTable";
 const spotify = new SpotifyWebApi();
 
 class User extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         const params = this.getHashParams();
         const token = params.access_token;
         console.log("token", token)
         this.state ={
           loggedIn: token ? true : false,
           tokens: params,
-          data:[],
+          userData:[],
           dataLoaded: false,
-          imageUrl: ""
+          imageUrl: "",
+          topTracks: [],
+          topTracksAttributes: []
+          
           
         }
         if (token) {
@@ -26,6 +30,9 @@ class User extends Component {
       }
       componentDidMount(){
           this.getUser();
+          this.getUserTopTracks();
+          
+         
           
       }
       
@@ -33,16 +40,64 @@ class User extends Component {
           spotify.getMe()
           .then(response => {
               this.setState({
-                  data: response,
-                  imageUrl: response.images[0].url
+                  userData: response,
+                  imageUrl: response.images[0].url,
+                  dataLoaded: true
               })
-              console.log(this.state)
+              
           })
           .catch(error => {
               console.log(error)
           })
           
       }
+      getUserTopTracks() {
+          spotify.getMyTopTracks({"limit":50, "offset":0, "time_range":"long_term"})
+          
+          .then((response)=>{
+              this.setState({
+                  topTracks: response})
+                console.log("toptracks", response)
+          })
+          .then(()=>{
+            this.getTopTracksIds();
+          })
+
+          .catch((error)=>{
+              console.log(error)
+          })
+      }
+
+      getTopTracksIds() {
+          var tracks = this.state.topTracks.items
+          const topTracksIds = tracks.map((track)=> {
+            return track.id
+          })
+        console.log(topTracksIds.toString())
+         spotify.getAudioFeaturesForTracks(topTracksIds).then((response)=>{
+             this.setState({
+                 topTracksAttributes: response.audio_features
+             })
+             console.log(this.state.topTracksAttributes)
+         }).then(()=>{
+            this.getSongName()
+         })
+
+
+      }
+      getSongName(){
+        console.log(this.state.topTracks.items[0].id)
+        console.log(this.state.topTracksAttributes[0].id)
+        for (let i = 0; i < this.state.topTracks.items.length; i ++){
+            for (let j = 0 ; j < this.state.topTracksAttributes.length; j++){
+                if (this.state.topTracks.items[i].id === this.state.topTracksAttributes[j].id){
+                    console.log(this.state.topTracks.items[i].name)
+                }
+            }
+        }
+        
+      }
+    
 
       getHashParams() {
         let hashParams = {};
@@ -53,8 +108,22 @@ class User extends Component {
         }
         return hashParams;
       }
+
   render() {
+      const tableRows = this.state.topTracksAttributes.map((tracks,i)=>(
+          <tr key={tracks.id}>
+              <th>{i+ 1}</th>
+              <th>{this.state.topTracks.items[i].name}</th>
+              <th>{this.state.topTracks.items[i].artists[0].name}</th>
+              <th>{(tracks.energy * 100).toFixed(0)}</th>
+              <th>{(tracks.danceability * 100).toFixed(0)}</th>
+              <th>{(tracks.valence * 100).toFixed(0)}</th>
+              <th>{(tracks.acousticness + 100).toFixed(0)}</th>
+              <th>{this.state.topTracks.items[i].popularity}</th>
+          </tr>
+      ))
       
+      console.log(this.state)
     if (this.state.loggedIn === false){
         return (
             <div className="user-page-logged-off">
@@ -68,12 +137,14 @@ class User extends Component {
             <div className="user-page-logged-in">
             <NavBar
             userImage = {this.state.imageUrl}
-            userName = {this.state.data.display_name}
+            userName = {this.state.userData.display_name}
+            />
+            <TracksTable
+            tracks = {tableRows}
             />
               
             </div>
           )
-
     }
     else {
         return (
