f9e11b1494ad9c81e313c63096dba5c001b4dd63
AlCast <alvaro.castelan.21@gmail.com>
2018-12-20 20:20:13 -0600

    remove console logs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import ReactGA from "react-ga";
 
 
 ReactGA.initialize(process.env.REACT_APP_GA_ID)
-console.log(process.env.GA_ID)
 ReactGA.pageview('/homepage');
 class App extends Component {
   
